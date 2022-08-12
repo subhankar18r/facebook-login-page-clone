322e2e63714fe3b4bc8738a1eabf60de1f6db9a6
01ricky18r <01ricky18r@gmail.com>
2022-08-12 15:41:15 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      md: "360px",
+      lg: "1000px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
